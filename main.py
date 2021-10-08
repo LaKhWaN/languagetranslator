6d9fb1952a70ce4fd60fd7a59783f155ac2e0662
LaKhWaN <lakhwanus009@gmail.com>
2021-10-08 12:13:15 +0530

    fixed minor bugs

--- main.py
+++ main.py
@@ -5,25 +5,35 @@ from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 from PyQt5 import QtWidgets,uic
 from PyQt5.QtCore import *
+
 app = QtWidgets.QApplication([])
 window = uic.loadUi("design.ui")
-print(LANGUAGES)
+
+window.setMaximumWidth(770)
+window.setMaximumHeight(406)
+window.setMinimumWidth(770)
+window.setMinimumHeight(406)
 for lang in LANGUAGES: # 'code':'full name'
     window.dropDown.addItem(LANGUAGES[lang].capitalize())
 translator = Translator(service_urls=['translate.googleapis.com'])
+
 def get_key(val):
     for key, value in LANGUAGES.items():
          if val.lower() == value:
              return key
+
 def changeLang(text,d):
     chng=translator.translate(text,d)
     return chng.text
+
 def changeBtn():
     text = window.textBox.toPlainText()
     dest = window.dropDown.currentText()
     window.textBox2.setPlainText(changeLang(text,get_key(dest)))
+
 def main():
     window.btn.clicked.connect(changeBtn)
+
 main()
 window.show()
 app.exec()
